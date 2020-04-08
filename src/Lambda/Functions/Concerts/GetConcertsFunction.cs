a0d689d5038f10d379d3aa1919c1fec998bc9049
BobvD <b.vandonselaar@student.fontys.nl>
2020-04-08 21:39:58 +0200

    Cors headers for Lambda endpoint

--- src/Lambda/Functions/Concerts/GetConcertsFunction.cs
+++ src/Lambda/Functions/Concerts/GetConcertsFunction.cs
@@ -1,7 +1,9 @@
 ﻿using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Amazon.Runtime.Internal.Transform;
 using Cloudbash.Application.Concerts.Queries.GetConcerts;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Cloudbash.Lambda.Functions.Concerts
@@ -13,7 +15,24 @@ namespace Cloudbash.Lambda.Functions.Concerts
         public async Task<APIGatewayProxyResponse> Run()
         {                   
             var result = await Mediator.Send(new GetConcertsQuery());
-            return new APIGatewayProxyResponse { StatusCode = 201, Body = JsonConvert.SerializeObject(result) };                       
+
+            return new APIGatewayProxyResponse
+            {
+                Headers = GetCorsHeaders(),
+                StatusCode = 201,
+                Body = JsonConvert.SerializeObject(result)
+            };
         }
+
+        private IDictionary<string, string> GetCorsHeaders()
+        {
+            return new Dictionary<string, string>()
+            {
+                new KeyValuePair<string, string>("Access-Control-Allow-Origin", "*"),
+                new KeyValuePair<string, string>("Access-Control-Allow-Credentials", "true"),
+                new KeyValuePair<string, string>("Access-Control-Allow-Methods", "OPTIONS,POST,GET")
+            };
+        }
+
     }
 }
