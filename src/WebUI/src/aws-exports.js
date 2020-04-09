b4a98701a9a9b48c32a105b837f55fcddf90f8dc
BobvD <b.vandonselaar@student.fontys.nl>
2020-04-09 21:04:54 +0200

    angular aws configuration

---
+++ src/WebUI/src/aws-exports.js
@@ -0,0 +1,11 @@
+const awsmobile = {
+    "aws_project_region": "eu-west-1",
+    "aws_cognito_identity_pool_id": "eu-west-1:40a05e2c-46fb-44ea-bb37-12e2a57b699c",
+    "aws_cognito_region": "eu-west-1",
+    "aws_user_pools_id": "eu-west-1_NWIg0h321",
+    "aws_user_pools_web_client_id": "59161ilhnmq1prjs33s16aa9ch",
+    "oauth": {}
+};
+
+
+export default awsmobile;
