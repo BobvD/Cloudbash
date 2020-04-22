121c5e2266ca165036ed190850d67f5583e991ec
BobvD <b.vandonselaar@student.fontys.nl>
2020-04-22 22:20:28 +0200

    update angular config

--- src/WebUI/src/aws-exports.js
+++ src/WebUI/src/aws-exports.js
@@ -2,8 +2,8 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_identity_pool_id": "",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_T8kEuCzBS",
-    "aws_user_pools_web_client_id": "1n4fkp5lo34mv7dqo35h9uhetc",
+    "aws_user_pools_id": "us-east-1_apS1zXSIG",
+    "aws_user_pools_web_client_id": "3ua2r40onpbm9uqbr450bve8nv",
     "oauth": {}
 };
 
