274733667fe6dd673f0744e37e8426f921cc8335
BobvD <b.vandonselaar@student.fontys.nl>
2020-05-11 06:24:37 +0200

    angular - update cognito user pool info

--- src/WebUI/src/aws-exports.js
+++ src/WebUI/src/aws-exports.js
@@ -2,8 +2,8 @@ const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_identity_pool_id": "",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_apS1zXSIG",
-    "aws_user_pools_web_client_id": "3ua2r40onpbm9uqbr450bve8nv",
+    "aws_user_pools_id": "us-east-1_h2TI5xYRE",
+    "aws_user_pools_web_client_id": "7gmnbtpr8uta4afirrold68vgr",
     "oauth": {}
 };
 
