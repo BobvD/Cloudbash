80cad8dc54c602706e22306f6751a28e270400c2
BobvD <b.vandonselaar@student.fontys.nl>
2020-06-04 02:01:22 +0200

    add user aggregate tests

--- src/Domain/Users/User.cs
+++ src/Domain/Users/User.cs
@@ -11,10 +11,11 @@ namespace Cloudbash.Domain.Users
         {
             Activities = new List<UserActivity>();
         }
-        
+
         public string FullName { get; set; }
         public string Email { get; set; }
         private List<UserActivity> Activities { get; set; }
+
         public User(Guid id, string fullName, string email)
         {
             AddEvent(new UserCreatedEvent(id, fullName, email));
@@ -22,7 +23,7 @@ namespace Cloudbash.Domain.Users
 
         public void AddActivityLog(UserActivityType activity)
         {
-            if(activity == UserActivityType.AUTHENTICATION)
+            if (activity == UserActivityType.AUTHENTICATION)
             {
                 AddEvent(new UserAuthenticatedEvent(Id));
             }
@@ -38,10 +39,11 @@ namespace Cloudbash.Domain.Users
         internal void Apply(UserAuthenticatedEvent ev)
         {
             Activities.Add(
-                new UserActivity { 
-                    Activity = UserActivityType.AUTHENTICATION, 
-                    UserID = ev.AggregateId, 
-                    TimeStamp = DateTime.Now 
+                new UserActivity
+                {
+                    Activity = UserActivityType.AUTHENTICATION,
+                    UserID = ev.AggregateId,
+                    TimeStamp = DateTime.Now
                 });
         }
     }
---
+++ tests/Domain.UnitTests/UserTests.cs
@@ -0,0 +1,46 @@
+﻿using Cloudbash.Domain.UnitTests.Common;
+using Cloudbash.Domain.Users;
+using Cloudbash.Domain.Users.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Cloudbash.Domain.UnitTests
+{
+    public class UserTests : AggregateRootBaseTest<User>
+    {
+        private readonly Guid userId = Guid.NewGuid();
+        private readonly Guid ticketTypeId = Guid.NewGuid();
+
+        [Fact]
+        public void When_Create_New_User_UserCreatedEvent()
+        {
+            var user = new User(userId, "John Doe", "john@mail.com");
+
+            AssertSingleUncommittedEvent<UserCreatedEvent>(user, @event =>
+            {
+                Assert.Equal(user.Id, @event.AggregateId);
+                Assert.Equal(user.FullName, @event.FullName);
+                Assert.Equal(user.Email, @event.Email);
+                Assert.NotEqual(default(Guid), @event.AggregateId);
+            });
+        }
+
+        [Fact]
+        public void Given_User_When_Activity_Of_Type_AUTHENTICATION_New_UserAuthenticetedEvent()
+        {
+            var user = new User(userId, "John Doe", "john@mail.com");
+
+            ClearUncommittedEvents(user);
+
+            user.AddActivityLog(UserActivityType.AUTHENTICATION);
+
+            AssertSingleUncommittedEvent<UserAuthenticatedEvent>(user, @event =>
+            {                
+                Assert.Equal(user.Id, @event.AggregateId);
+            });
+        }
+
+    }
+}
