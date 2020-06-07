ecfd7c063e3ea00f3e7b7398edc8710d802a8574
BobvD <b.vandonselaar@student.fontys.nl>
2020-06-07 03:16:53 +0200

    in-memory event store

---
+++ src/Infrastructure/EventStore/InMemoryEventStore.cs
@@ -0,0 +1,32 @@
+﻿using Cloudbash.Domain.SeedWork;
+using Cloudbash.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cloudbash.Infrastructure.EventStore
+{
+    public class InMemoryEventStore : IEventStore
+    {
+        private readonly List<IDomainEvent> Events;
+
+
+        public InMemoryEventStore()
+        {
+            Events = new List<IDomainEvent>();
+        }
+
+        public Task<IEnumerable<IDomainEvent>> GetAsync(Guid aggregateId, long minVersion, long maxVersion)
+        {
+            var result = Events.Where(e => e.AggregateId == aggregateId).OrderBy(x => x.AggregateVersion);
+            return Task.FromResult(result.AsEnumerable());
+        }
+
+        public Task SaveAsync(IDomainEvent @event)
+        {
+            Events.Add(@event);
+            return Task.CompletedTask;
+        }
+    }
+}
